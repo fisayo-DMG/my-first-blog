8003100ab86ee263cc30c07a20779d989c94171c
fisayo-DMG <aikomofisayo@gmail.com>
2016-07-14 19:29:42 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post # the dot means current directory or current
+	# application. so techinically this means from the same directory
+	# that has views.py, import models (no need to .py to models)
+from django.utils import timezone
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts})
+
