0888ecaf58bf3a08e6a821bd566ed8d49da09a0f
fisayo-DMG <aikomofisayo@gmail.com>
2016-07-15 06:50:53 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post # the dot means current directory or current
 	# application. so techinically this means from the same directory
 	# that has views.py, import models (no need to .py to models)
@@ -8,3 +8,6 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
